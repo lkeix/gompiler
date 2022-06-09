d2c36639e1c146b6f09d3aeab19b3a3064d1c048
lkeix <ai.kanaria.ai@gmail.com>
2022-06-09 22:26:25 +0900

    add: assemble decleared func

--- main.go
+++ main.go
@@ -4,19 +4,35 @@ import (
 	"fmt"
 	"go/ast"
 	"go/parser"
+	"go/token"
 	"strconv"
 )
 
+const MAIN = "main"
+
 // AT&T syntax
-func emitOSExit() {
-	fmt.Printf(".text\n")                  // start of text section
-	fmt.Printf(".global _start\n")         // .global label: _start can be called from other files
-	fmt.Printf("_start:\n")                // _start label: entry point
-	fmt.Printf("  callq main.main\n\n")    // call main.main
+func osExit() {
+	fmt.Printf(".text\n")
 	fmt.Printf("os.Exit:\n")               // os.Exit label: exit
 	fmt.Printf("  movq 8(%%rsp), %%rdi\n") // rsp(stack pointer register) + 8 address  value(42(decimal) = 2a(hex)) to rdi(destination register)
 	fmt.Printf("  movq $60, %%rax\n")      // rax(accumulator register) = 60
 	fmt.Printf("  syscall\n\n")            // emit syscall
+}
+
+func runtime() {
+	fmt.Printf("# runtime\n")
+	fmt.Printf(".global _start\n")
+	fmt.Printf("_start:\n")
+	fmt.Printf("  callq main.main\n\n")
+}
+
+func generate(file *ast.File) {
+	for _, decl := range file.Decls {
+		switch decl.(type) {
+		case *ast.FuncDecl:
+			emitDeclFunc(MAIN, decl.(*ast.FuncDecl))
+		}
+	}
 }
 
 func emitExpr(expr ast.Expr) {
@@ -74,7 +90,25 @@ func emitFunc(expr *ast.CallExpr) {
 		fmt.Printf("  popq %%rax\n")
 		fmt.Printf("  pushq %%rax\n")
 		symbol := fmt.Sprintf("%s.%s", fn.X, fn.Sel)
-		fmt.Printf("  call %s\n", symbol)
+		fmt.Printf("  callq %s\n", symbol)
+	}
+}
+
+// emitDeclFunc emits assembly code for a declarated function. parse func XXX(...) {...}
+func emitDeclFunc(pkg string, DeclFunc *ast.FuncDecl) {
+	fmt.Printf("# %T\n", DeclFunc)
+	fmt.Printf(".text\n")
+	fmt.Printf("%s.%s:\n", pkg, DeclFunc.Name)
+
+	// emit assembly code for function body. parse {...}
+	for _, stmt := range DeclFunc.Body.List {
+		switch stmt.(type) {
+		case *ast.ExprStmt:
+			expr := stmt.(*ast.ExprStmt).X
+			emitExpr(expr)
+		default:
+			must(fmt.Errorf("unexpected stmt type %T", stmt))
+		}
 	}
 }
 
@@ -85,18 +119,16 @@ func must(err error) {
 }
 
 func main() {
-	source := "os.Exit(1 + 2 * (20 + 1) - 1)"
-	expr, err := parser.ParseExpr(source)
+	// define runtime and os.Exit
+	runtime()
+	osExit()
+
+	// define file set
+	fset := token.NewFileSet()
+	// parse source from source/main.go
+	f, err := parser.ParseFile(fset, "./source/main.go", nil, parser.ParseComments)
 	must(err)
 
-	emitOSExit()
-
-	fmt.Printf(".text\n")
-	fmt.Printf(".globl main\n")
-	fmt.Printf("main.main:\n")
-	emitExpr(expr)
-	fmt.Printf("  popq %%rax\n")  // pop rax value and increament rs
-	fmt.Printf("  pushq %%rax\n") // decrement rsp and push rax value to rsp
-	// fmt.Printf("  callq os.Exit\n") // decrement (rsp address) - 8 and write next rip address(os.Exit) to rsp address
-	fmt.Printf("  ret\n")
+	// generate assembly code
+	generate(f)
 }
