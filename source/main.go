99aa48c3d715011da0f9a789b60e7680169c490f
lkeix <ai.kanaria.ai@gmail.com>
2022-06-19 00:12:46 +0900

    update: target source

--- source/main.go
+++ source/main.go
@@ -2,18 +2,34 @@ package main
 
 import "os"
 
-var hoge int = 30
-var fuga int = 12
-var errorMessage string = "to stderr\n"
+var (
+	globalstring string = "I'm global string\n"
+	globalint1   int    = 30
+	globalint2   int    = 0
+)
+
+func f1(x int) int {
+	return x + 1
+}
 
 func main() {
-	var a int
-	var str string
-	a = 1
-	str = "aaa"
-	print(a)
-	print(str)
-	print("hello world\n")
-	print(errorMessage)
-	os.Exit(hoge + fuga)
+	globalint2 = f1(1)
+
+	var localstring1 string
+
+	print("Start!\n")
+	localstring1 = "I'm local string1\n"
+	print(globalstring)
+	print(localstring1)
+	localstring1 = "local string1 changed\n"
+	globalstring = "globalstring changed\n"
+	print(localstring1)
+	print(globalstring)
+
+	var localint1 int
+	localint1 = 10
+
+	print("end!\n")
+
+	os.Exit(globalint1 + globalint2 + localint1)
 }
